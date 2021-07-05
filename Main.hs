82bcacb2cfa5d4023a7e9318dca1d53bcbdb4795
marina <mfuster@itba.edu.ar>
2021-07-05 17:03:14 -0300

    :bulb: ADD: main comments so experiment works as a template

--- Main.hs
+++ Main.hs
@@ -7,35 +7,35 @@ import Evaluation ( hits, classify )
 
 -- | Experiment example
 
-experiment :: IO ()
-experiment = do
+experimentTemplate :: IO ()
+experimentTemplate = do
 
     -- provision of dataset
-    samples <- loadMatrix "datasets/iris_x.dat"
-    targets <- loadMatrix "datasets/iris_y.dat"
+
+    samples <- loadMatrix "datasets/iris_x.dat"  -- PARAM: change datasets/iris_x.dat for another dataset
+    targets <- loadMatrix "datasets/iris_y.dat"  -- PARAM: change datasets/iris_y.dat for true values
 
     -- provision of initial net
-    let inputFeatures = 4
-        layers = [128, 3]
-        activations = [Relu, Sigmoid]
+
+    let inputFeatures = 4                        -- PARAM: quantity of columns of datasets/iris_x.dat
+        layers = [128, 3]                        -- PARAM: quantity of neurons for each layer of your nn
+        activations = [Relu, Sigmoid]            -- PARAM: activation function for each layer of your nn
     
     net <- buildNetwork inputFeatures layers activations
 
     -- provision of train parameters
-    let learningRate = 0.001
-        optimizer = GradientDescent learningRate
-        epochs = 7000
+
+    let learningRate = 0.001                     -- PARAM: learning rate for your optimizer
+        optimizer = GradientDescent learningRate -- PARAM: optimizer for your nn
+        epochs = 7000                            -- PARAM: for how many epochs will the nn train
         
         trainedNet = train net optimizer (samples, targets) epochs
-
 
     print $ takeRows 10 (feedforward trainedNet samples)
     print $ takeRows 10 (classify trainedNet samples)
 
-    --let correct = hits trainedNet (samples, targets)
-    --putStrLn $ printf "Hits for net (gradient descent) %.2f" (correct)
 
-main = experiment
+main = experimentTemplate
 
 -- | TODO: figure out a way to compute time properly and compare it
 -- | to python way of doing it ??
