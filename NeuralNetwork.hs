34ca97410acea503d7f88b50e9dc6bc9766a2e35
marina <mfuster@itba.edu.ar>
2021-07-05 14:07:58 -0300

    :sparkles: ADD: backprop and feedfw

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -47,13 +47,73 @@ applyDerivative Sigmoid = \x dY -> sigmoidGradient x * dY
 applyDerivative Relu = \x dY -> reluGradient x * dY
 applyDerivative Tanh = \x dY -> tanhGradient x * dY
 
-compute :: Error -> Matrix Double -> Matrix Double -> Matrix Double
--- | TODO: test
--- sumElements will return a single value instead of MSE for each sample
+compute :: Error -> Matrix Double -> Matrix Double -> Double
 compute MSE = \x y -> sumElements $ 0.5 `scale` (cmap (^ 2) (x - y))  
 
 computeDerivative :: Error -> Matrix Double -> Matrix Double -> Matrix Double
--- | TODO: test
 computeDerivative MSE = \x y -> x - y
 
--- | TODO: recursive function for backprop and feedforward
+-- | Backpropagation and Feed Forward algorithms
+
+pass :: 
+  Matrix Double ->                            -- targets
+  Matrix Double ->                            -- inputs
+  NeuralNetwork ->                            -- layers
+  (Matrix Double, Matrix Double, [Gradients]) -- (dX, predictions, list of gradients)
+
+pass t inp [] = computeLoss t inp
+pass t inp (Layer w b a:xs) = (dX, preds, gradient:gradients)
+  where
+    h = getH inp w b
+    (dZ, preds, gradients) = pass t (apply a h) xs
+    (dX, gradient) = computeGradients inp h dZ (Layer w b a)
+
+backprop :: 
+  NeuralNetwork ->                    -- nn
+  (Matrix Double, Matrix Double) ->   -- (samples, targets)
+  (Matrix Double, [Gradients])        -- (predictions, list of gradients)
+
+backprop net (samples, targets) = dropFirst $ pass targets samples net 
+
+forward ::
+  NeuralNetwork ->      -- nn
+  Matrix Double ->      -- samples
+  Matrix Double         -- predictions
+
+forward net samples = fst $ backprop net (samples, undefined) 
+
+-- | Util functions
+
+computeLoss :: Matrix Double -> Matrix Double -> (Matrix Double, Matrix Double, [Gradients])
+computeLoss t inp = (computeDerivative MSE inp t, inp, [])
+
+getH :: Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
+getH inp w b = (inp LA.<> w) + b
+
+computeGradients :: 
+  Matrix Double ->                  -- inputs 
+  Matrix Double ->                  -- excitation of layer
+  Matrix Double ->                  -- dZ from next layer
+  Layer ->                          -- current layer
+  (Matrix Double, Gradients)
+computeGradients inp h dZ (Layer w b a) = (linearX' w dY, Gradients dW dB)
+  where
+    dY = applyDerivative a h dZ
+    (dW, dB) = (linearW' inp dY, bias' dY)
+
+bias' :: Matrix Double -> Matrix Double
+bias' dY = cmap (/ m) r
+  where
+    r = matrix (cols dY) $ map sumElements (toColumns dY)
+    m = fromIntegral $ rows dY
+
+linearW' :: Matrix Double -> Matrix Double -> Matrix Double
+linearW' x dY = cmap (/ m) (tr' x LA.<> dY)
+  where
+    m = fromIntegral $ rows x
+
+linearX' :: Matrix Double -> Matrix Double -> Matrix Double
+linearX' w dY = dY LA.<> tr' w
+
+dropFirst :: (a, b, c) -> (b, c)
+dropFirst (x, y, z) = (y, z)
